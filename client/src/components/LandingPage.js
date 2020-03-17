90fe98538ada730643a994d499c49e0d8a585135
Li <54919246+lienaquesada@users.noreply.github.com>
2020-03-17 19:51:23 -0400

    Update LandingPage.js

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -38,52 +38,75 @@ function LandingPage() {
         <div className="section section-about-us">
           <Container>
             <Row>
-              <Col className="ml-auto mr-auto text-center" md="8">
-                <h2 className="title">Add Your Business Details</h2>
+              <Col className="text-center" md="12">
+                <h2 className="title">Business Details</h2>
                 <div class="row">
-                  <form class="col s12">
+                  <form class="col s6">
                     <div class="row">
-                      <div class="input-field col s3">
-                        <i class="material-icons prefix">account_circle</i>
-                        <input id="icon_prefix" type="text" class="validate" />
-                        <label for="icon_prefix">Company</label>
+                    <div class="input-field col s6">
+                        <label for="textarea1"> Business Name</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                        <label for="textarea1">Address</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                        <label for="textarea1">Zip Code</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                        <label for="textarea1">Username</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                        <label for="textarea1">Image Url</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                      </div>
+                      <div class="input-field col s6">                        
+                        <div class="input-field col s6">
+                        <label for="textarea1">Owner</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
                       </div>
                       <div class="input-field col s3">
-                        <i class="material-icons prefix">phone</i>
-                        <input id="icon_telephone" type="tel" class="validate" />
-                        <label for="icon_telephone">Telephone</label>
+                        <label for="textarea1">Website</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
                       </div>
-                      <div class="input-field col s3">
-                        <i class="material-icons prefix">devices</i>
-                        <input id="icon_prefix" type="text" class="validate" />
-                        <label for="icon_prefix">Website</label>
+                      <div class="input-field col s6">
+                        <label for="textarea1">Phone Number</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
                       </div>
-                    </div>
-                  </form>
-                </div>
-
-                <div class="row">
-                  <form class="col s12">
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input placeholder="Address" id="first_name" type="text" class="validate" />
-                        <label for="textarea1">Location</label>
+                      <div class="input-field col s6">
+                        <label for="textarea1">Email</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                      </div>
+                      <div class="input-field col s6">
+                        <label for="textarea1">Current Location</label>
+                        <input placeholder="" id="first_name" type="longtext" class="form-control"/>
+                      </div>
                       </div>
                     </div>
                   </form>
                 </div>
                 <form class="col s12">
                   <div class="row">
-                    <div class="input-field col s12">
-                      <input placeholder="Friday 6pm-2am Saturday 8pm-3am" id="first_name" type="text" class="validate" />
-                      <label for="textarea1">Hours of Operation</label>
-                    </div>
+                    <div class="input-field col s4"> 
+                    <br/>
+                    <h4 className="title">Hours of Operation</h4>                  
+                    <label for="textarea1">Monday</label> <br/>
+                      <input placeholder="" id="first_name" type="text" class="form" /> <br/>
+                      <label for="textarea1">Tuesday</label> <br/>
+                      <input placeholder=" " id="first_name" stype="text" class="form" /> <br/>
+                      <label for="textarea1">Wednesday</label> <br/>
+                      <input placeholder=" " id="first_name" type="text" class="form" /> <br/>
+                      <label for="textarea1">Thursday</label> <br/>
+                      <input placeholder=" " id="first_name" type="text" class="form" /><br/>
+                      <label for="textarea1">Friday</label><br/>
+                      <input placeholder=" " id="first_name" type="text" class="form" /><br/>
+                      <label for="textarea1">Saturday</label><br/>
+                      <input placeholder=" " id="first_name" type="text" class="form" /><br/>
+                      <label for="textarea1">Sunday</label><br/>
+                      <input placeholder=" " id="first_name" type="text" class="form" /><br/> 
+                     </div>
                   </div>
+                  <button type="submit" class="btn btn-info">Submit</button>
                 </form>
               </Col>
             </Row>
+            <a href="">Go Back</a>
             <div className="separator separator-primary"></div>
-
           </Container>
         </div>
         <DefaultFooter />
